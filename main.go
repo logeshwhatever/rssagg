591501f03cc814c05bb0c9d9767dc3067858388a
logeshwhatever <logeshwhatever@gmail.com>
2024-12-12 11:00:25 +0000

    B: Set up basic boilerplate for HTTP server

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	
+
+	godotenv.Load()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("Port is not available")
+	}
+
+	fmt.Println("Port:", port)
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Server starting on port %v", port)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
